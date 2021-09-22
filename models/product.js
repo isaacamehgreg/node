da2999521b807517b367046c280a38fe90c9db8c
Isaac Ameh <67932483+isaacamehgreg@users.noreply.github.com>
2021-09-22 20:20:13 +0100

    push

--- models/product.js
+++ models/product.js
@@ -1,37 +1,37 @@
 const fs = require('fs');
 const path = require('path');
-const products = [];
-const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-    
-    constructor(t) {
-        this.title = t;
-    }
+  constructor(t) {
+    this.title = t;
+  }
 
-    save() { 
-        fs.readFile(p,'utf8', (err,data)=>{
-            console.log(data);
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-            if (!err) {
-              return  products = JSON.parse(data);
-            }
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err)=>{
-                console.log(err);
-            })
-        })
-    }
-
-    static fetchAll(cb){
-        fs.readFile(p, (err,data)=> {
-            if(err){
-                cb([])
-            }
-            return cb(JSON.parse(data));
-        });
-      
-
-
-    }
-}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
