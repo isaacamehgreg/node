1a4d68cbdd42f7da0270d5411c4efbe99eefa3bf
Isaac Ameh <67932483+isaacamehgreg@users.noreply.github.com>
2021-09-22 13:39:39 +0100

    exports

--- routes/admin.js
+++ routes/admin.js
@@ -2,28 +2,17 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productController = require('../controllers/products')
 
 const router = express.Router();
 
 const products = [];
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-});
+router.get('/add-product', productController.addProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
+router.post('/add-product', );
 
 exports.routes = router;
 exports.products = products;
