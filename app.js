aaebafeda7ca1a0b63825ce31715f1668d7b5914
Isaac Ameh <67932483+isaacamehgreg@users.noreply.github.com>
2021-09-23 22:50:11 +0100

    sequelize

--- app.js
+++ app.js
@@ -14,7 +14,13 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-db.execute('')
+// db.execute('SELECT * FROM products')
+// .then((data) => {
+//     console.log(data[0][1])
+// })
+// .catch((err) =>{
+//     console.log(err);
+// });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
